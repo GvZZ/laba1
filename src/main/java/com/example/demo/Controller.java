06c601cdd8e4efbeaf21f0037590c174316560fe
hijiri <zemeon228@gmail.com>
2024-02-19 22:08:02 +0700

    ?

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class Controller {
 
@@ -13,7 +16,22 @@ public class Controller {
     private URL location;
 
     @FXML
+    private Label timer;
+
+    @FXML
+    void Up_Timer(KeyEvent event) {
+        if(event.getCode() == KeyCode.T) {
+            if (timer.isVisible()) {
+                timer.setVisible(false);
+            } else {
+                timer.setVisible(true);
+            }
+        }
+    }
+
+    @FXML
     void initialize() {
+        assert timer != null : "fx:id=\"timer\" was not injected: check your FXML file 'hello-view.fxml'.";
 
     }
 
