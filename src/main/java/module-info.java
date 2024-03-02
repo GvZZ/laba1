15a9314012f5abf771eeb2bf3d146cabe2b4ee58
Wave <gluthizhsol@mail.ru>
2024-03-02 20:43:11 +0700

    перенос на мэйн

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
-module com.example.demo {
+module com.example.laba1_test {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
 
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    opens com.example.laba1_test to javafx.fxml;
+    exports com.example.laba1_test;
 }
